37f369e823140992553f325faaf41bc791799a78
陈 玉龙 <chendamalong@chenmatoMacBook-Pro.local>
2012-08-08 00:20:19 +0800

    Initial Commit

---
+++ AC_PAZZCK/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AC_PAZZCK
+//
+//  Created by 陈 玉龙 on 12-8-8.
+//  Copyright (c) 2012年 陈 玉龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
